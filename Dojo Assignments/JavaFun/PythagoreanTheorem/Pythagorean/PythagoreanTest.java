746b98a285b0c60e6511bea2374defb15e47444e
Antonio Gutierrez <antonioguteirrz@gmail.com>
2021-04-06 07:11:56 -0700

    git push

--- Dojo Assignments/JavaFun/PythagoreanTheorem/Pythagorean/PythagoreanTest.java
+++ Dojo Assignments/JavaFun/PythagoreanTheorem/Pythagorean/PythagoreanTest.java
@@ -4,7 +4,7 @@
 public class PythagoreanTest {
     public static void main(String[] args) {
         Pythagorean che = new Pythagorean();
-        double answer = che.calculateHypotenuse(20, 4);
-        System.out.printf("This i the answer: %s", answer);
+        double answer = che.calculateHypotenuse(20, 20);
+        System.out.printf("This is the answer: %s", answer);
     }
 }
