455c2012e854fe628f906602fc6dfb4499a45133
Antonio Gutierrez <antonioguteirrz@gmail.com>
2021-04-06 07:02:04 -0700

    git push

---
+++ Dojo Assignments/JavaFun/FizzBuzz/FizzBuzzTest.java
@@ -0,0 +1,11 @@
+
+
+public class FizzBuzzTest {
+    public static void main(String[] args){
+        FizzBuzz rest = new FizzBuzz();
+        String answer = rest.fizzBuzz(5);
+        System.out.println(answer);
+
+    }
+    
+}
