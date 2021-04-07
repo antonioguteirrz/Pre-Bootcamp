f2bb71d9b26b53002b4465592fb2ac06d253370e
Antonio Gutierrez <antonioguteirrz@gmail.com>
2021-04-07 15:27:26 -0700

    git push

--- DojoAssignments/JavaFun/Basics/Basics.java
+++ DojoAssignments/JavaFun/Basics/Basics.java
@@ -2,13 +2,46 @@ package DojoAssignments.JavaFun.Basics;
 
 public class Basics{
     public void printNumber(int number){
-        for(int i = 0; i<number;i++){
+        for(int i = 1; i<number;i++){
         System.out.println(i);}
     }
 
     public void printOddNumber(int number){
-        for(int i = 0; i<number;i++){
-
+        for(int i = 1; i<number;i++){
+            if(i%2 == 1){
+                System.out.println(i);
+            }
         }
+    }
+    public void printNumberSum(int number){
+        int sum = 0;
+        for(int i = 1; i<number;i++){
+            sum = sum +i;
+            System.out.printf("number:%s, Sum:%s \n",i,sum);
+        }
+    }
+    public void printArray(int arr[]){
+        for(int i = 0; i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    public void printArrayMax(int arr[]){
+        int max = arr[0];
+        for(int i = 0; i<arr.length;i++){
+            if(max< arr[i]){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+    public void printArrayAverage(int arr[]){
+        int sum = 0;
+        for(int i = 0; i<arr.length;i++){
+            sum+=arr[i];
+            
+        }
+        int average = sum/arr.length;
+        System.out.println(average);
+
     }
 }
--- DojoAssignments/JavaFun/Basics/BasicsTest.java
+++ DojoAssignments/JavaFun/Basics/BasicsTest.java
@@ -3,8 +3,16 @@ package DojoAssignments.JavaFun.Basics;
 
 public class BasicsTest{
     public static void main (String args[]){
-        System.out.println("Hello World");
+        Basics numbers = new Basics();
+        //numbers.printNumber(256);
 
-    }
+    //numbers.printOddNumber(256);
+    // numbers.printNumberSum(256);
+    int [] x ={2,10,3,5,100};
+    //numbers.printArray(x);
+    //numbers.printArrayMax(x);
+    numbers.printArrayAverage(x);
+
+}
     
 }
