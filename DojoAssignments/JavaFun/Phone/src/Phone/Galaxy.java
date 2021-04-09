c7ca4042ac2d7d1502f938f07c5aff00ed9372bc
Antonio Gutierrez <antonioguteirrz@gmail.com>
2021-04-09 12:30:15 -0700

    git push

---
+++ DojoAssignments/JavaFun/Phone/src/Phone/Galaxy.java
@@ -0,0 +1,21 @@
+package Phone;
+
+public class Galaxy extends Phone implements Ringable {
+    public Galaxy(String versionNumber, int batteryPercentage, String carrier, String ringTone) {
+        super(versionNumber, batteryPercentage, carrier, ringTone);
+    }
+    @Override
+    public String ring() {
+        return "Galaxy "+ getVersionNumber() + " says " + getRingTone();
+    }
+    @Override
+    public String unlock() {
+        return "unlocking via Galaxy fingerprint";
+    }
+    @Override
+    public void displayInfo() {
+        System.out.println("Galaxy "+ getVersionNumber()+ " from " + getCarrier());  
+    }
+}
+
+
