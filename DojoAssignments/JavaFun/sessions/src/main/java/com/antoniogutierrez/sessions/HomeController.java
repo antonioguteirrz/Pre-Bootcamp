7e5aaf64028bcffafedb46589f48d4cdd2e8a171
Antonio Gutierrez <antonioguteirrz@gmail.com>
2021-04-16 07:02:01 -0700

    git push

--- DojoAssignments/JavaFun/sessions/src/main/java/com/antoniogutierrez/sessions/HomeController.java
+++ DojoAssignments/JavaFun/sessions/src/main/java/com/antoniogutierrez/sessions/HomeController.java
@@ -3,6 +3,7 @@ package com.antoniogutierrez.sessions;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,15 +12,24 @@ public class HomeController {
 
 	@RequestMapping("")
     public String index(HttpSession session) {
-    	session.setAttribute("count", +1);
-        return "home.jsp";
+    	if(session.getAttribute("counter")== null){
+    		session.setAttribute("counter",0);
+    		}
+    	else {
+    		Integer sessionCounter =(Integer) session.getAttribute("counter");
+    		sessionCounter++;
+    		session.setAttribute("counter", sessionCounter);
+        
     }
-	public void testing(HttpSession session) {
-		
-		session.setAttribute("count", +1);
-	}
+    	return "home.jsp";
+    	}
+	
     @RequestMapping("/counter")
-    public String session(HttpSession session){
-        return "counter.jsp";
+    public String session(Model model, HttpSession session){
+    	Integer counter = (Integer) session.getAttribute("counter");
+    	model.addAttribute("visits", counter);
+    	return "counter.jsp";
+
+    	}
+
     }
-}
--- DojoAssignments/JavaFun/sessions/src/main/webapp/WEB-INF/counter.jsp
+++ DojoAssignments/JavaFun/sessions/src/main/webapp/WEB-INF/counter.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    
+    <%@ taglib prefix = "c" uri = "http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,7 +9,8 @@
 <title>Insert title here</title>
 </head>
 <body>
-${sessionScope['count']} 
+<h1> <c:out value="${visits}"/></h1>
+
 <a href = "http://localhost:8080/your_server">Test another visit?</a>
 </body>
 </html>
